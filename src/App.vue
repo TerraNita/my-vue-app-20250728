43600128f51e3675b463f4a249bb3489533665c4
YUSUKE NITAHARA <ynitahara@terrasky.com>
2025-08-07 20:13:39 +0900

    App.vueのナビゲーションリンクを更新し、HomeViewとAboutViewに変更。ルーティング機能の整合性を向上。

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
     <ParentComponent />
   </div>
   <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">HomeView</router-link> |
+      <router-link to="/aboutView">AboutView</router-link>
     </nav>
     <router-view/>
 </template>
